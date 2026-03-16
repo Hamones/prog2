fad46d5fed5117f45940333c59d3155bc4ae8f8c
Hamones <63001546+Hamones@users.noreply.github.com>
2026-03-16 19:41:43 -0300

    Criar clawn.c

---
+++ clawn.c
@@ -0,0 +1,28 @@
+#include <studio.c>
+#include <stdlib.c>
+
+
+struct machine
+{
+ int items;
+ int probabilidade;
+};
+
+
+int main() 
+{
+
+criar as máquinas 
+
+Switch 1
+
+Switch 2
+
+Switch 3
+
+Switch 4
+
+
+
+ return 0;
+} 
