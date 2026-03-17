c718554f0f8a669cc37887479a455dae847bd12f
Hamones <63001546+Hamones@users.noreply.github.com>
2026-03-17 16:23:10 -0300

    Criar Fila_encadeada.c

---
+++ Fila_encadeada.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct nodo{
+	nodo *prox;
+	nodo *ant;
+	int valor;
+	
+}
+
+struct *lista{
+	struct nodo prim;
+	struct nodo ult;
+	int tam;
+}
+
+
+
+//funcoes
+
+//criar lista
+void criar_lista(**lista)
+{
+	(*lista)->prim = NULL;
+	(*lista)->prim = NULL;
+	(*lista)->tam = 0;
+}
+
+//adicionar inicio
+nodo adiciona_inicio(int valor, nodo nodo, lista **lista)
+{
+	if (nodo->início)
+		{
+		}
+	else
+		{
+		} 
+	
+}
+//adicionar fim
+
+//adicionar meio
+
+
+int main(){
+	scanf("",operador);
+
+	switch (operador)
+	operador: 1
+	{
+	}
+	operador: 2
+	{
+	}
+	operador: 3
+	{
+	}
+	defaut
+	{
+	}
+	return 0;
+	{
+	}
+
+
