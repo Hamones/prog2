8ac9e21927ee2eb07bb084836d83993f5f64df2b
Hamones <63001546+Hamones@users.noreply.github.com>
2026-03-22 02:17:54 -0300

    Criar Teste22.c

---
+++ Teste22.c
@@ -0,0 +1,23 @@
+struct nodo{
+	int valor;
+	struct nodo prox;
+	
+};
+
+
+struct nodo inserir_inicio(struct *nodo nodo, int valor)
+{
+	struct nodo *aux;
+
+	//em qual momento utilizo o malloc?
+	//qual é o podeiro que devo adicionar os aux?
+
+	If (nodo)
+		aux->valor = valor;
+	eles
+			
+		
+
+
+
+}
