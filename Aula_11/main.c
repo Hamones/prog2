ec2ba23c8ac1511aebda30c67739814f1728bc82
Hamones <63001546+Hamones@users.noreply.github.com>
2026-04-15 18:36:52 -0300

    Add files via upload

---
+++ Aula_11/main.c
@@ -0,0 +1,6 @@
+#include "escreva.h"
+int main ()
+{
+	escreva ("Hello, world!\n");
+	return (0);
+}
---
+++ Aula_11/teste_makefile/makefile/main.c
@@ -0,0 +1,7 @@
+#include "greeter.h"
+
+int main(void)
+{
+	greeter("wwwwhuman");
+	return 0;
+}
